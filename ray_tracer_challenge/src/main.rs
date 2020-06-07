f721588d0219e4321a5606de334c28d2c310db05
Wayne Stewart <waynegaidin@hotmail.com>
2020-06-07 11:51:41 -0500

    mag, norm, dot, cross implemented

--- ray_tracer_challenge/src/main.rs
+++ ray_tracer_challenge/src/main.rs
@@ -112,13 +112,26 @@ fn fequal(a: f32, b: f32) -> bool {
     }
 }
 
-fn point(x: f32, y: f32, z: f32) -> Tuple {
+fn tuple(x: f32, y: f32, z: f32, w: f32) -> Tuple {
     Tuple {
         x,
         y,
         z,
-        w: 1.0
+        w
     }
+}
+
+fn tuple_i(x: i32, y: i32, z: i32, w: i32) -> Tuple {
+    Tuple {
+        x: x as f32,
+        y: y as f32,
+        z: z as f32,
+        w: w as f32
+    }
+}
+
+fn point(x: f32, y: f32, z: f32) -> Tuple {
+    tuple(x, y, z, 1.0)
 }
 
 fn point_i (x: i32, y: i32, z: i32) -> Tuple {
@@ -126,26 +139,38 @@ fn point_i (x: i32, y: i32, z: i32) -> Tuple {
 }
 
 fn vector(x: f32, y: f32, z: f32) -> Tuple {
-    Tuple {
-        x,
-        y,
-        z,
-        w: 0.0
-    }
+    tuple(x, y, z, 0.0)
 }
 
 fn vector_i(x: i32, y: i32, z: i32) -> Tuple {
     vector(x as f32, y as f32, z as f32)
 }
 
-// fn add(a: Tuple, b: Tuple) -> Tuple {
-//     Tuple {
-//         x: a.x + b.x,
-//         y: a.y + b.y,
-//         z: a.z + b.z,
-//         w: a.w + b.w
-//     }
-// }
+fn magnitude(v: &Tuple) -> f32 {
+    (v.x * v.x + v.y * v.y + v.z * v.z + v.w * v.w).sqrt()
+}
+
+fn normalize(v: &Tuple) -> Tuple {
+    let mag = magnitude(&v);
+    tuple(
+        v.x / mag,
+        v.y / mag,
+        v.z / mag,
+        v.w / mag
+    )
+}
+
+fn dot(a: &Tuple, b: &Tuple) -> f32 {
+    a.x * b.x + a.y * b.y + a.z * b.z + a.w * b.w
+}
+
+fn cross(a: &Tuple, b: &Tuple) -> Tuple {
+    vector(
+        a.y * b.z - a.z * b.y,
+        a.z * b.x - a.x * b.z,
+        a.x * b.y - a.y * b.x
+    )
+}
 
 #[test]
 fn point_test() {
@@ -215,16 +240,58 @@ fn negation_test() {
 
 #[test]
 fn multiply_test() {
-    let a = Tuple{x: 1.0, y: -2.0, z: 3.0, w: -4.0 };
+    let a = tuple_i(1, -2, 3, -4);
     let t = a * 3.5;
-    let r = Tuple{x: 3.5, y: -7.0, z: 10.5, w: -14.0 };
+    let r = tuple(3.5, -7.0, 10.5, -14.0);
     assert_eq!(r, t);
 }
 
 #[test]
 fn divide_test() {
-    let a = Tuple{x: 1.0, y: -2.0, z: 3.0, w: -4.0 };
+    let a = tuple_i(1, -2, 3, -4);
     let t = a / 2.0;
-    let r = Tuple{x: 0.5, y: -1.0, z: 1.5, w: -2.0 };
+    let r = tuple(0.5, -1.0, 1.5, -2.0);
     assert_eq!(r, t);
+}
+
+#[test]
+fn magnitude_test() {
+    let v = vector_i(-1,-2,-3);
+    let t = magnitude(&v);
+    let r = (14.0 as f32).sqrt();
+    assert_eq!(r, t);
+}
+
+#[test]
+fn normalize_test() {
+    let v = vector_i(4,0,0);
+    let t = normalize(&v);
+    let r = vector_i(1,0,0);
+    assert_eq!(r, t);
+
+    let v = vector_i(1, 2, 3);
+    let t = normalize(&v);
+    let r = vector(0.26726, 0.53452, 0.80178);
+    assert_eq!(r, t);
+}
+
+#[test]
+fn dot_test() {
+    let v1 = vector_i(1,2,3);
+    let v2 = vector_i(2,3,4);
+    let t = dot(&v1, &v2);
+    let r = 20.0;
+    assert_eq!(r, t);
+}
+
+#[test]
+fn cross_test() {
+    let v1 = vector_i(1,2,3);
+    let v2 = vector_i(2,3,4);
+    let t = cross(&v1, &v2);
+    let s = cross(&v2, &v1);
+    let rt = vector_i(-1,2,-1);
+    let rs = vector_i(1,-2,1);
+    assert_eq!(rt, t);
+    assert_eq!(rs, s);
 }
