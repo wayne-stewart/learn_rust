62e5a91f997148506d83ec3fdee8e83608a48d8f
Wayne Stewart <waynegaidin@hotmail.com>
2021-05-31 17:27:17 -0500

    basic game is working

--- game_of_life/src/lib.rs
+++ game_of_life/src/lib.rs
@@ -11,14 +11,22 @@ use std::cell::RefCell;
 
 enum Cell {
     Alive,
-    Dead
+    Dead,
+
+    // these are transitional values
+    Dying,      // still alive for the purposes of cell calculations
+    Reviving    // still dead for the purposes of cell calculations
 }
 
-// struct Canvas {
-//     width: usize,
-//     height: usize,
-//     data: Vec<u8>
-// }
+impl Cell {
+    pub fn is_alive(&self) -> usize {
+        match self {
+            Cell::Alive => 1,
+            Cell::Dying => 1,
+            _ => 0
+        }
+    }
+}
 
 struct GameState {
     canvas_width: usize,
@@ -28,6 +36,7 @@ struct GameState {
     cell_row_count: usize,
     cell_col_count: usize,
     cells: RefCell<Vec<Cell>>,
+    buffer: RefCell<Vec<u8>>,
     mouse_x: usize,
     mouse_y: usize,
     mouse_x_max: usize,
@@ -44,6 +53,7 @@ impl GameState {
             cell_row_count: 0,
             cell_col_count: 0,
             cells: RefCell::new(Vec::with_capacity(0)),
+            buffer: RefCell::new(Vec::with_capacity(1024)),
             mouse_x: 0,
             mouse_y: 0,
             mouse_x_max: 0,
@@ -62,17 +72,96 @@ impl GameState {
         let mut cells = self.cells.borrow_mut();
         match cells[i] {
             Cell::Alive => { cells[i] = Cell::Dead; },
-            Cell::Dead => { cells[i] = Cell::Alive; }
+            Cell::Dead => { cells[i] = Cell::Alive; },
+            _ => { }
+        }
+    }
+
+    pub fn kill_all(&mut self) {
+        let mut cells = self.cells.borrow_mut();
+        for i in 0..cells.len() {
+            cells[i] = Cell::Dead;
+        }
+    }
+
+    pub fn is_alive(&self, x: usize, y: usize) -> bool {
+        let cells = self.cells.borrow();
+        match cells[y * self.cell_col_count + x].is_alive() {
+            1 => true,
+            _ => false
+        }
+    }
+
+    pub fn die(&mut self, x: usize, y: usize) {
+        let mut cells = self.cells.borrow_mut();
+        cells[y * self.cell_col_count + x] = Cell::Dying;
+    }
+
+    pub fn revive(&mut self, x: usize, y: usize) {
+        let mut cells = self.cells.borrow_mut();
+        cells[y * self.cell_col_count + x] = Cell::Reviving;
+    }
+
+    pub fn count_living_neighbours(&self, x: usize, y: usize) -> usize {
+        let x = x as i32;
+        let y =  y as i32;
+        let cells = self.cells.borrow();
+        let stride = self.cell_col_count;
+        let xlen = self.cell_col_count as i32;
+        let ylen = self.cell_row_count as i32;
+        let pairs: [(i32,i32); 8] = [
+            (x-1, y-1),
+            (x,   y-1),
+            (x+1, y-1),
+            (x-1, y),
+            (x+1, y),
+            (x-1, y+1),
+            (x,   y+1),
+            (x+1, y+1)
+            ];
+        let mut count = 0;
+        for coord in pairs.iter() {
+            if coord.0 >= 0 && coord.0 < xlen &&
+               coord.1 >= 0 && coord.1 < ylen {
+                   count += cells[coord.1 as usize * stride + coord.0 as usize].is_alive();
+               }
+        }
+        count
+    }
+
+    pub fn tick(&mut self) {
+        // each alive cell with 1 or 0 alive neighbors dies
+        // each alive cell with 4+ alive neighbors dies
+        // each dead cell with 3 alive neighbors lives
+
+        for y in 0..self.cell_row_count {
+            for x in 0..self.cell_col_count {
+                let nc = self.count_living_neighbours(x, y);
+                let living = self.is_alive(x, y);
+                if living && (nc < 2 || nc > 3) {
+                    self.die(x, y);
+                }
+                else if !living && nc == 3 {
+                    self.revive(x, y);
+                }
+            }
+        }
+
+        // finalize alive or dead
+        let mut cells = self.cells.borrow_mut();
+        for i in 0..cells.len() {
+            cells[i] = match cells[i] {
+                Cell::Dying    => Cell::Dead,
+                Cell::Reviving => Cell::Alive,
+                Cell::Alive    => Cell::Alive,
+                Cell::Dead     => Cell::Dead
+            };
         }
     }
 }
 
 lazy_static! {
     static ref GAME_STATE: Mutex<GameState> = Mutex::new(GameState::new());
-}
-
-pub fn tick() -> i32 { 
-    4002
 }
 
 #[derive(Clone)]
@@ -122,6 +211,8 @@ fn render_fill_rect(canvas_data: &mut Vec<u8>, canvas_width: usize, canvas_heigh
 #[no_mangle]
 pub extern "C" fn render() {
     let state = &mut GAME_STATE.lock().unwrap();
+    let mut canvas_data = state.canvas_data.borrow_mut();
+    let cells = state.cells.borrow();
     let width = state.canvas_width;
     let height = state.canvas_height;
     let cell_size = state.cell_size;
@@ -130,20 +221,18 @@ pub extern "C" fn render() {
     let col_count = state.cell_col_count;
     let mouse_x = state.mouse_x;
     let mouse_y = state.mouse_y;
-    let mut canvas_data = state.canvas_data.borrow_mut();
-    render_fill_rect(&mut canvas_data, width, height, Color::GREY, 0, 0, width, height);
-    // render_fill_rect(state, Color::BLACK, 0, 0, width, 2);
-    // render_fill_rect(state, Color::BLACK, 0, 0, 2, height);
-    // render_fill_rect(state, Color::BLACK, width - 2, 0, 2, height);
-    // render_fill_rect(state, Color::BLACK, 0, height - 3, width, 2);
 
+    // draw grey background
+    render_fill_rect(&mut canvas_data, width, height, Color::GREY, 0, 0, width, height);
+
+    // draw the current cell the mouse is hovering over blue
     let cell_hover_x = (mouse_x as f32 / cell_size) as usize;
     let cell_hover_y = (mouse_y as f32 / cell_size) as usize;
     let cell_hover_x = (cell_hover_x as f32 * cell_size) as usize;
     let cell_hover_y = (cell_hover_y as f32 * cell_size) as usize;
     render_fill_rect(&mut canvas_data, width, height, Color::BLUE, cell_hover_x + 1, cell_hover_y + 1, cell_size_usize, cell_size_usize);
 
-    let cells = state.cells.borrow();
+    // draw all the Alive cells yellow
     for y in 0..row_count {
         for x in 0..col_count {
             let cell_index = y * col_count + x;
@@ -158,17 +247,19 @@ pub extern "C" fn render() {
         }
     }
 
+    // draw the horizontal grid lines
     for y in 0..row_count {
         render_fill_rect(&mut canvas_data, width, height, Color::BLACK, 0, ((y+1) as f32 * cell_size) as usize, width, 1);
     }
 
+    // draw the vertical grid lines
     for x in 0..col_count {
         render_fill_rect(&mut canvas_data, width, height, Color::BLACK, ((x+1) as f32 * cell_size) as usize, 0, 1, height);
     }
 }
 
 #[no_mangle]
-pub extern "C" fn init_rgba_canvas(width: usize, height: usize) -> *const u8 {
+pub extern "C" fn init_rgba_canvas(width: usize, height: usize) {
     let state = &mut GAME_STATE.lock().unwrap();
     state.canvas_width = width;
     state.canvas_height = height;
@@ -182,25 +273,31 @@ pub extern "C" fn init_rgba_canvas(width: usize, height: usize) -> *const u8 {
         state.cell_row_count = 100;
         state.cell_col_count = (width as f32 / state.cell_size) as usize;
     }
-    //state.cells = Vec::with_capacity(state.cell_col_count * state.cell_row_count);
-    state.cells = RefCell::new((0..(state.cell_col_count * state.cell_row_count))
-        .map(|_x| Cell::Dead)
-        .collect());
+
     state.mouse_x_max = (state.cell_col_count as f32 * state.cell_size) as usize - 2;
     state.mouse_y_max = (state.cell_row_count as f32 * state.cell_size) as usize - 2;
 
-    // init all pixels to white
-    let mut canvas_data = state.canvas_data.borrow_mut();
-    for _y in 0..height {
-        for _x in 0..width {
-            canvas_data.push(0xFF);
-            canvas_data.push(0xFF);
-            canvas_data.push(0xFF);
-            canvas_data.push(0xFF);
-        }
-    }
+    state.cells = RefCell::new((0..(state.cell_col_count * state.cell_row_count))
+        .map(|_x| Cell::Dead)
+        .collect());
 
-    canvas_data.as_ptr()
+    state.canvas_data = RefCell::new((0..(width * height * 4))
+        .map(|_x| 0xFF)
+        .collect());
+}
+
+#[no_mangle]
+pub extern "C" fn get_canvas_ptr() -> *const u8 {
+    let state = &mut GAME_STATE.lock().unwrap();
+    let ptr = state.canvas_data.borrow().as_ptr(); 
+    ptr
+}
+
+#[no_mangle]
+pub extern "C" fn get_buffer_ptr() -> *const u8 {
+    let state = &mut GAME_STATE.lock().unwrap();
+    let ptr = state.buffer.borrow().as_ptr(); 
+    ptr
 }
 
 #[no_mangle]
@@ -226,4 +323,36 @@ pub fn onclick(x: usize, y: usize) {
     state.mouse_x = std::cmp::min(std::cmp::max(x, 2), state.mouse_x_max);
     state.mouse_y = std::cmp::min(std::cmp::max(y, 2), state.mouse_y_max);
     state.toggle_cell();
+}
+
+#[no_mangle]
+pub fn clear() {
+    let state = &mut GAME_STATE.lock().unwrap();
+    state.kill_all();
+}
+
+#[no_mangle]
+pub fn tick() { 
+    let state = &mut GAME_STATE.lock().unwrap();
+    state.tick();
+}
+
+#[no_mangle]
+pub fn export() {
+    // let state = &mut GAME_STATE.lock().unwrap();
+    // let cells = state.cells.borrow();
+    // let buffer = state.buffer.borrow_mut();
+    // for y in 0..row_count {
+    //     for x in 0..col_count {
+    //         let cell_index = y * col_count + x;
+    //         match cells[cell_index] {
+    //             Cell::Alive => { 
+    //                 let y = (y as f32 * cell_size) as usize;
+    //                 let x = (x as f32 * cell_size) as usize;
+    //                 render_fill_rect(&mut canvas_data, width, height, Color::YELLOW, x + 1, y + 1, cell_size_usize, cell_size_usize);
+    //             },
+    //             _ => { }
+    //         }
+    //     }
+    // }
 }
